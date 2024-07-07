9ffb960dd80e929cf37f4a6916167f693e5e34d9
Vinit Keshri <vinitkeshri2020@gmail.com>
2024-07-07 23:55:52 +0530

    Changes routes

--- frontend/notes-app/src/App.jsx
+++ frontend/notes-app/src/App.jsx
@@ -10,6 +10,7 @@ import {BrowserRouter as Router , Routes, Route} from 'react-router-dom'
 const routes = (
   <Router>
     <Routes>
+      <Route path="/" element={<Home />} />
       <Route path="/dashboard" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<SignUp />} />
